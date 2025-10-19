cc384eeca5c48ad7383cba00947acc8d37da076b
Sajan Toor <sajantoor98@gmail.com>
2025-10-18 18:10:15 -0700

    Compute header checksum

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -261,8 +261,6 @@ static NEW_LICENSE_CODES: LazyLock<HashMap<&'static str, &'static str>> = LazyLo
     ])
 });
 
-enum NewLicenseCode {}
-
 impl Cartridge {
     pub fn from_file(path: &str) -> io::Result<Self> {
         let rom_data = fs::read(path)?;
@@ -278,8 +276,22 @@ impl Cartridge {
         })
     }
 
-    pub fn validate_checksum() -> bool {
-        panic!("Not implemented yet");
+    pub fn read() {
+        panic!("Not implemented yet!");
+    }
+
+    pub fn validate_header_checksum(&self) -> bool {
+        let mut computed_checksum: u8 = 0;
+
+        for i in 0x0134..(0x014C + 1) {
+            computed_checksum = computed_checksum.wrapping_sub(self.rom_data[i] + 1);
+        }
+
+        if computed_checksum != self.rom_header.header_checksum {
+            panic!("Header checksum is invalid!!!");
+        }
+
+        return true;
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod cartridge;
 use crate::cartridge::Cartridge;
 
 fn main() {
-    let cartridge = Cartridge::from_file("./roms/Tetris.gb");
-    println!("{:?}", cartridge.unwrap().rom_header);
+    let cartridge_result = Cartridge::from_file("./roms/Tetris.gb");
+    let cartridge = cartridge_result.unwrap();
+    println!("{:?}", cartridge.rom_header);
+    println!("{:?}", cartridge.validate_header_checksum())
 }
