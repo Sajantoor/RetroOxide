1a816cf7631f48a33ee3ff10e1d94b0ea005c222
Sajan Toor <sajantoor98@gmail.com>
2025-11-08 21:59:14 -0800

    Take an argument for the cartridge to load instead of hard coded path

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@ mod cartridge;
 use crate::cartridge::Cartridge;
 
 fn main() {
-    let cartridge_result = Cartridge::from_file("./roms/Tetris.gb");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        panic!("Not enough arguments provided");
+    }
+
+    let file_path = args.get(1).unwrap();
+
+    let cartridge_result = Cartridge::from_file(&file_path);
     let cartridge = cartridge_result.unwrap();
+
     println!("{:?}", cartridge.rom_header);
     println!("{:?}", cartridge.validate_header_checksum())
 }
